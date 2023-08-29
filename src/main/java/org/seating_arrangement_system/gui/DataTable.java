88d0fb2d30c3ed2f4bd5d68978ed48bd7e3e71fa
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-08-29 09:41:37 +0545

    printing seatplan

--- src/main/java/org/seating_arrangement_system/gui/DataTable.java
+++ src/main/java/org/seating_arrangement_system/gui/DataTable.java
@@ -4,11 +4,16 @@ import org.seating_arrangement_system.db.models.Seat;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.util.List;
 import java.util.Vector;
 
-public class DataTable extends JFrame {
-
+public class DataTable extends JFrame implements ActionListener {
+    private JTable table;
     DataTable(List<Seat> data, String[] headers) {
 
         setSize(700, 300);
@@ -27,14 +32,42 @@ public class DataTable extends JFrame {
             model.addRow(row);
         }
 
-        JTable table = new JTable(model);
+        table = new JTable(model);
 
         JScrollPane scrollPane = new JScrollPane(table);
 
         add(scrollPane);
+        addButton();
+    }
+    private void addButton() {
+        JButton printButton = new JButton("Print");
+        printButton.addActionListener(this); // Register the button with the ActionListener
+        add(printButton, BorderLayout.SOUTH); // Add the button to the bottom of the frame
     }
 
     void render() {
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Handle the button click event here
+        if (e.getActionCommand().equals("Print")) {
+            try {
+                // Create a PrinterJob
+                PrinterJob printerJob = PrinterJob.getPrinterJob();
+
+                // Set the printable content to the JTable
+                printerJob.setPrintable(table.getPrintable(JTable.PrintMode.FIT_WIDTH, null, null));
+
+                // Show the print dialog to the user
+                if (printerJob.printDialog()) {
+                    // If the user confirms, start the printing process
+                    printerJob.print();
+                }
+            } catch (PrinterException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
