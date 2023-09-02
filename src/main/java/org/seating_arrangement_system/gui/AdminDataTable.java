909d0df963d91b033f40734764b2645a06f26bb8
Kushzz18 <kushalpathak18@gmail.com>
2023-09-02 17:59:38 +0545

    modified

--- src/main/java/org/seating_arrangement_system/gui/AdminDataTable.java
+++ src/main/java/org/seating_arrangement_system/gui/AdminDataTable.java
@@ -1,19 +1,17 @@
-
-
-
 package org.seating_arrangement_system.gui;
 
-        import org.seating_arrangement_system.db.models.Seat;
+import org.seating_arrangement_system.db.models.Seat;
 
-        import javax.swing.*;
-        import javax.swing.table.DefaultTableModel;
-        import java.awt.*;
-        import java.awt.event.ActionEvent;
-        import java.awt.event.ActionListener;
-        import java.awt.print.PrinterException;
-        import java.awt.print.PrinterJob;
-        import java.util.List;
-        import java.util.Vector;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.util.List;
+import java.util.Vector;
+
 
 public class AdminDataTable extends JFrame implements ActionListener{
     private JTable table;
@@ -22,16 +20,12 @@ public class AdminDataTable extends JFrame implements ActionListener{
         setSize(700, 300);
 
         DefaultTableModel model = new DefaultTableModel();
-        model.setColumnIdentifiers(headers);
+        model.setColumnIdentifiers(new String[] { "Student Id", "Seat Id" });
 
         for (Seat seat : data) {
             Vector<Object> row = new Vector<>();
             row.add(seat.getStudentId());
-            row.add(seat.getStudentName());
-            row.add(seat.getHallInfo());
-            row.add(seat.getRoomNo());
             row.add(seat.getSeatId());
-            row.add(seat.getSemester());
             model.addRow(row);
         }
 
